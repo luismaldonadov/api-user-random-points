e50960b7070ddb1519e3c35abfb5e352bd386057
Luis Maldonado <maldonadovla@gmail.com>
2020-11-13 08:10:57 -0600

    Add test

--- test/user_random_points/users_test.exs
+++ test/user_random_points/users_test.exs
@@ -2,6 +2,7 @@ defmodule UserRandomPoints.UsersTest do
   use UserRandomPoints.DataCase
 
   alias UserRandomPoints.Users
+  alias UserRandomPoints.Users.User
 
   describe "Users domain" do
     test "returns all users using get_all/0" do
@@ -18,6 +19,11 @@ defmodule UserRandomPoints.UsersTest do
     test "returns a changeset error when updating a User using get_users_gt_points/1" do
       [user | _rest] = Users.get_all()
       assert {:error, %Ecto.Changeset{}} = Users.update_user(user, %{points: "A"})
+    end
+
+    test "returns an updated user when updating a User using get_users_gt_points/1" do
+      [user | _rest] = Users.get_all()
+      assert {:ok, %User{}} = Users.update_user(user, %{points: 99})
     end
   end
 end
