cb6b5042d62ff10d6fc510aab018885d67c70e8f
Luis Maldonado <maldonadovla@gmail.com>
2020-11-13 08:05:17 -0600

    Add Users seed on test
    Add ecto drop for database on test

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,8 @@
 ExUnit.start()
+
+Enum.map(1..10, fn _ ->
+  random_points = :rand.uniform(100)
+  UserRandomPoints.Repo.insert!(%UserRandomPoints.Users.User{points: random_points})
+end)
+
 Ecto.Adapters.SQL.Sandbox.mode(UserRandomPoints.Repo, :manual)
--- test/user_random_points_web/views/error_view_test.exs
+++ test/user_random_points_web/views/error_view_test.exs
@@ -5,7 +5,9 @@ defmodule UserRandomPointsWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(UserRandomPointsWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(UserRandomPointsWeb.ErrorView, "404.json", []) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500.json" do
