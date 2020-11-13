62d0b19bcf6b48bd7870ee0ac4ebad6720b9dde9
Luis Maldonado <maldonadovla@gmail.com>
2020-11-12 21:46:32 -0600

    Add user seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Enum.map(1..100, fn _ ->
+  random_points = :rand.uniform(100)
+  UserRandomPoints.Repo.insert!(%UserRandomPoints.Users.User{points: random_points})
+end)
