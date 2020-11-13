5a63a65c112c871cd04e33bcf671c723f2f81e42
Luis Maldonado <maldonadovla@gmail.com>
2020-11-12 22:32:12 -0600

    Update users points periodically

--- lib/user_random_points/users/operation_handler.ex
+++ lib/user_random_points/users/operation_handler.ex
@@ -1,7 +1,12 @@
 defmodule UserRandomPoints.Users.OperationHandler do
   use GenServer
+  require Logger
+
+  alias UserRandomPoints.Repo
+  alias UserRandomPoints.Users.User
 
   @max_number 100
+  @update_users_points_after_ms 60 * 1000
 
   def start_link(_) do
     random_max_number = :rand.uniform(@max_number)
@@ -13,6 +18,24 @@ defmodule UserRandomPoints.Users.OperationHandler do
 
   @impl true
   def init(state) do
+    Process.send_after(self(), :update_users_points, @update_users_points_after_ms)
     {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:update_users_points, state) do
+    modified_users =
+      User
+      |> Repo.all()
+      |> Enum.each(fn user ->
+        random_point_number = :rand.uniform(@max_number)
+
+        user
+        |> User.changeset(%{points: random_point_number})
+        |> Repo.update()
+      end)
+
+    Process.send_after(self(), :update_users_points, @update_users_points_after_ms)
+    {:noreply, Map.put(state, :max_number, :rand.uniform(@max_number))}
   end
 end
