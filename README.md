4f38b3060223b1896e9eb6ffe62e5b07eb314075
Luis Maldonado <maldonadovla@gmail.com>
2020-11-13 09:55:40 -0600

    Add missing test

--- README.md
+++ README.md
@@ -6,8 +6,10 @@
   - OTP 23.0
 
 ## Enviroment variables
-1.- Inside the root folder you will find an .env sample file with the neccesary structure file
+1.- Inside the root folder you will find an ```.env.sample``` file with the neccesary structure file
 needed to create the ```.env``` file
 
 2.- After you created an ```.env ``` file in the root folder with
 the needed enviroment variables you can procced with the next steps.
+
+## How to run
--- test/user_random_points_web/controllers/user_random_points_web_controller_test.exs
+++ test/user_random_points_web/controllers/user_random_points_web_controller_test.exs
@@ -12,5 +12,26 @@ defmodule UserRandomPointsWeb.UserRandomPointsControllerTest do
       assert %{"users" => users} = json_response(conn, 200)
       assert length(users) <= 2
     end
+
+    test "returns users with timestamp that is not nil", %{conn: conn} do
+      conn =
+        get(
+          conn,
+          Routes.user_random_points_path(conn, :show)
+        )
+
+      conn =
+        get(
+          conn,
+          Routes.user_random_points_path(conn, :show)
+        )
+
+      # By making 2 requests, we guarantee that timestamp is not nil
+      # regardelss of test order
+
+      assert %{"users" => users, "timestamp" => timestamp} = json_response(conn, 200)
+      assert length(users) <= 2
+      assert {:ok, %DateTime{}, 0} = DateTime.from_iso8601(timestamp)
+    end
   end
 end
