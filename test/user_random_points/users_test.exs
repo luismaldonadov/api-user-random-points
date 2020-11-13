6c6c4fac837b832cee009f523b420a606d9bb33e
Luis Maldonado <maldonadovla@gmail.com>
2020-11-13 08:07:18 -0600

    Add test

--- test/user_random_points/users_test.exs
+++ test/user_random_points/users_test.exs
@@ -6,7 +6,13 @@ defmodule UserRandomPoints.UsersTest do
   describe "Users domain" do
     test "returns all users on get_all/0" do
       users = Users.get_all()
+      # Amount of users generated on test helper
       assert length(users) == 10
+    end
+
+    test "returns at the most 2 users on get_users_gt_points/1" do
+      users = Users.get_users_gt_points(0)
+      assert length(users) <= 2
     end
   end
 end
