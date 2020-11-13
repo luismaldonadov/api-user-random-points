62fd375206e96cb91a1982f69d3cbfa3c30ff8dd
Luis Maldonado <maldonadovla@gmail.com>
2020-11-13 08:18:53 -0600

    Reduce pattern match destructure

--- test/user_random_points/operation_handler_test.exs
+++ test/user_random_points/operation_handler_test.exs
@@ -5,7 +5,7 @@ defmodule UserRandomPoints.Users.OperationHandlerTest do
 
   describe "get_users_max_number" do
     test "returns a list with 2 Users at the most, and a timestamp" do
-      %{timestamp: _timestamp, users: users} = OperationHandler.get_users_max_number()
+      %{users: users} = OperationHandler.get_users_max_number()
 
       assert length(users) <= 2
     end
