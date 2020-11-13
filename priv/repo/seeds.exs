c0cae3e4926cf022e2d84afb7392341c98c6fcb3
Luis Maldonado <maldonadovla@gmail.com>
2020-11-12 21:47:08 -0600

    Remove boilerplate comment

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,14 +1,6 @@
 # Script for populating the database. You can run it as:
 #
 #     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     UserRandomPoints.Repo.insert!(%UserRandomPoints.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
 
 Enum.map(1..100, fn _ ->
   random_points = :rand.uniform(100)
